d91b994df4b4b69ca660ba141a8c8786b575ea6e
PHISAN SANTITAMNONT <phisan.chula@gmail.com>
2024-05-22 17:28:55 +0700

    Add files via upload

--- BaselinePlanning/GNSS_Planning.py
+++ BaselinePlanning/GNSS_Planning.py
@@ -25,6 +25,7 @@ class Network:
                        self.PNTS[['STA','geometry']] ] )
 
     def ReadPnt(self,KML ):
+        ''' read points from [network] , make station name "P-1,P-2,P-3" etc. '''
         df = gpd.read_file(KML , driver='KML')
         df = df[['Name','geometry']].copy()
         df['Pnt'] = df.index+1
@@ -38,6 +39,8 @@ class Network:
         self.PNTS = gpd.GeoDataFrame( df , crs='EPSG:4326', geometry=df.geometry )
 
     def CreateBaseLine( self ):
+        ''' create baseline from string of point , estimate kilometer and duration
+            in minute for GNSS baseline observation '''
         dfs = list()
         for line,pnts in self.TOML['network'].items():
             LIN = list()
@@ -56,62 +59,84 @@ class Network:
             km = f'{km:.1f} km'
             return km,mins
         self.gdfBL[['km','mins']] = self.gdfBL.apply( CalcBL, axis=1, result_type='expand' )
-
         pnts = pd.DataFrame( nw.gdfBL[['FR','TO']].values.flatten() )
         cors = pnts[~pnts[0].str.startswith('P-') ]
         cors = self.dfCORS[ self.dfCORS.STA.isin( cors[0].tolist() ) ]
         self.CORS = cors[['STA','X','Y','Z','Lat','Long','h','epoch']]
         # GeoidEval -n "egm2008-1" --haetomsl --input-string "14 100 -56"
-        #import pdb ; pdb.set_trace()
 
-    def BaseLineSession( self):
+    def _CORS_exists(self, BL_SESS ):
+        ''' check if ['P-1', 'CORS' ]  in CORS ? '''
+        return self.CORS['STA'].isin( BL_SESS ).any()
+
+    def _CORS_remove(self, STA_SESS ):
+        ''' remove CORS form list e.g. remove 'AWLK' from
+             [P-27, P-25, AWLK, P-12] and return [P-27, P-25, P-12] '''
+        assert len( self.CORS[ self.CORS['STA'].isin( STA_SESS )] ) == 1
+        CORS = self.CORS[ self.CORS['STA'].isin( STA_SESS )].iloc[0].STA
+        STA = pd.Series( STA_SESS )
+        return STA[STA != CORS ]
+
+    def BLSession_Occupation( self ):
+        ''' generate indepent baseline Rovers + CORS '''
         GNSS_RCV = self.TOML['receiver']
         INDEP_BL = len( GNSS_RCV )-1
-        DAY = 1
-        CLK_BEG = pd.Timestamp( self.TOML['begin'] ) 
         SESS = list()
-        for trav,baseline in self.gdfBL.groupby( 'Loop' ):
+        for loop,baseline in self.gdfBL.groupby( 'Loop' ):
             #print( f'=============={trav}===============' )
             FULL,REST = divmod( len(baseline),INDEP_BL)
             STA = pd.Series( baseline[['FR','TO']].values.flatten() ).unique()
-            #print( f'{FULL} ->  {REST}')
-            for isess in range( FULL+1 ):
-                if isess==FULL and REST==0: break
-                elif isess==FULL and REST>0: gnss_rcv = GNSS_RCV[0:REST+1]
-                else: gnss_rcv = GNSS_RCV
-                fr = INDEP_BL*isess
+            ibl = 0
+            while ibl<len(baseline):
+                fr = ibl
                 to = fr + INDEP_BL    # last index could over-run!
-                rcv_line = baseline.iloc[fr:to] 
-                #print( rcv_line)
-                #import pdb ; pdb.set_trace()
-                rcv_sta = pd.Series( rcv_line[['FR','TO']].values.flatten() ).unique()
-                assert len(rcv_sta)==len(gnss_rcv)
-                DURAT = rcv_line['mins'].max() 
+                rcv_bl = baseline.iloc[fr:to] 
+                rcv_sta = pd.Series( rcv_bl[['FR','TO']].values.flatten() ).unique()
+                if self._CORS_exists( rcv_sta ):
+                    fr = ibl
+                    to = fr + INDEP_BL + 1    # last index could over-run!
+                    sess_bl  = baseline.iloc[fr:to] 
+                    baselines = pd.Series( sess_bl[['FR','TO']].values.flatten() ).unique()
+                    sta_occu = self._CORS_remove( baselines )
+                else:
+                    baselines = rcv_sta
+                    sta_occu = rcv_sta
+                #print( rcv_bl )
+                SESS.append(  [ loop, rcv_bl['mins'].max(), sta_occu.tolist(), baselines.tolist() ]  )  
+                ibl = to
+        self.SESS = pd.DataFrame( SESS, columns=['Loop', 'duration', 'sta_occu', 'baselines' ] )
+        #import pdb ; pdb.set_trace()
+
+    def BLSessionCLK( self):
+        DAY = 1
+        CLK_BEG = pd.Timestamp( self.TOML['begin'] ) 
+        clk = list()
+        for loop_name, loop in self.SESS.groupby( 'Loop' ):
+            #print( f'=============={loop}===============' )
+            for i, row in loop.iterrows(): 
+                DURAT = row['duration']
                 CLK_END = CLK_BEG + pd.Timedelta( minutes=DURAT)
-                SESS.append( [ trav, DAY, CLK_BEG.strftime('%H:%M'), 
-                                          CLK_END.strftime('%H:%M'), DURAT ] + rcv_sta.tolist()  )  
-                #print( rcv_line )
-                CLK_BEG = CLK_BEG + pd.Timedelta( minutes=DURAT) + pd.Timedelta( minutes=self.TOML['travel'] )
+                clk.append( [ DAY, CLK_BEG.strftime('%H:%M'), CLK_END.strftime('%H:%M') ] )
+                CLK_BEG = CLK_BEG + pd.Timedelta( minutes=DURAT) +\
+                                     pd.Timedelta( minutes=self.TOML['travel'] )
                 if CLK_END>=pd.Timestamp(self.TOML['end']):
                     CLK_BEG = pd.Timestamp( self.TOML['begin'] )
                     DAY = DAY+1
-        self.SESS = pd.DataFrame( SESS, columns=['Loop', 'Day', 'Begin','End', 'Duration', *GNSS_RCV ] )
+        self.SESS[['day','clk_beg','clk_end']] = clk
+        self.SESS = self.SESS[['Loop',  'day', 'duration', 'clk_beg', 'clk_end', 
+                               'sta_occu',  'baselines' ]] 
+        #import pdb ; pdb.set_trace()
 
     def GenLocation(self):
-        RCV = self.TOML['receiver']
         locs = list()
         for i,row in self.SESS.iterrows():
             LOC = list()
-            for i in row[RCV]:
-                if i is None: 
-                    name = '-'
-                elif i in list(self.PNTS.STA):
-                    name = self.PNTS[self.PNTS.STA==i].iloc[0].Name
-                else:
-                    name = '*CORS*'
+            for i in row.sta_occu:
+                name = self.PNTS[self.PNTS.STA==i].iloc[0].Name
                 LOC.append( name )
             locs.append( LOC )
         self.SESS['locations'] = locs
+        #import pdb ; pdb.set_trace()
 
     def PlotGPKG(self):
         GPKG = "./CACHE/RockArt_Baseline.gpkg"
@@ -134,14 +159,15 @@ print( f'Work Hour:  {nw.TOML["begin"]} to {nw.TOML["end"]}' )
 print( f'Transport time:  {nw.TOML["travel"]} minutes' )
 nw.CreateBaseLine()
 nw.PlotGPKG()
-nw.BaseLineSession()
+nw.BLSession_Occupation()
+nw.BLSessionCLK( )
 nw.GenLocation()
-print( '=========================== GNSS Occupation Session ========================' )
+nw.SESS.rename( columns={ 'sta_occu':  str(nw.TOML['receiver']) }, inplace=True )
+print( '=== GNSS Occupation Session ===' )
 COLS = nw.SESS.columns
 print( nw.SESS[COLS[:-1]].to_markdown()  )
-
-FMT =(None,None,'15.3f','15.3f','15.3f','^20s' ) 
-print( nw.SESS[COLS[-4:]].to_markdown()  )
-
-print( nw.CORS[['STA', 'X','Y','Z','epoch']].to_markdown( floatfmt=FMT ) )
 import pdb ;pdb.set_trace()
+if 0:
+    FMT =(None,None,'15.3f','15.3f','15.3f','^20s' ) 
+    print( nw.SESS[COLS[-4:]].to_markdown()  )
+    print( nw.CORS[['STA', 'X','Y','Z','epoch']].to_markdown( floatfmt=FMT ) )
